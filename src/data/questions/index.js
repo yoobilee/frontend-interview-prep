c422866000a5e8eeaeb6911b9243d4aeafcade0a
yoobilee <yblrrto23@gmail.com>
2026-05-15 15:03:55 +0900

    chore: 질문 데이터 파일 제거 - Supabase DB로 완전 이전

--- src/data/questions/index.js
+++ src/data/questions/index.js
@@ -1,10 +1,3 @@
-import { htmlCssQuestions } from './html-css'
-import { javascriptQuestions } from './javascript'
-import { reactQuestions } from './react'
-import { browserNetworkQuestions } from './browser-network'
-import { csQuestions } from './cs'
-import { performanceQuestions } from './performance'
-
 export const categories = [
   { id: 'html-css', label: 'HTML/CSS' },
   { id: 'javascript', label: 'JavaScript' },
@@ -12,13 +5,4 @@ export const categories = [
   { id: 'browser-network', label: '브라우저/네트워크' },
   { id: 'cs', label: 'CS 기초' },
   { id: 'performance', label: '성능 최적화' },
-]
-
-export const questions = [
-  ...htmlCssQuestions,
-  ...javascriptQuestions,
-  ...reactQuestions,
-  ...browserNetworkQuestions,
-  ...csQuestions,
-  ...performanceQuestions,
 ]
